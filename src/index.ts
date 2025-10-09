28820abca7dfdec0a142dc8500a7ee9afe031622
LC <the_speaker3@protonmail.com>
2025-10-09 14:38:27 +0200

    fix: Dependency deprecation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import '@/css/index.css';
-
 export { default as ThemeSwitcher } from '@/components/ThemeSwitcher';
 export { default as ThemeProvider } from '@/components/ThemeProvider';
 export { useThemeContext } from '@/hooks/State';
